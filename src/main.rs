87adb1f9c2d06e4e98c4d5b7d35d2b3082bc9b2f
François Triquet <francois.triquet@recast.ai>
2017-02-20 16:16:51 +0100

    add clap

--- src/main.rs
+++ src/main.rs
@@ -29,43 +29,71 @@ fn main() {
         parents: None,
     };
 
-    // let matches = App::new("Npuzzle")
-    //     .about("Taquin solver")
-    //     .subCommand(SubCommand::with_name("generate")
-    //                 .about("generates random game configuration")
-    //                 .arg(Arg::with_name("size")
-    //                      .short("s")
-    //                      .long("size")
-    //                      .value_name("SIZE"))
-    //                 .arg(Arg::with_name("solvable")
-    //                      .long("solvable")
-    //                      .value_name("SOLVABLE"))
-    //                 .arg(Arg::with_name("iterations")
-    //                      .short("i")
-    //                      .long("iterations")
-    //                      .value_name("ITERATIONS")))
-    //     .subCommand(SubCommand::with_name("solve")
-    //                 .about("solves random game configuration")
-    //                 .arg(Arg::with_name("file")
-    //                      .index(1)
-    //                      .value_name("FILE"))).get_matches();
+    let matches = App::new("Npuzzle")
+        .about("Taquin solver")
+        .subcommand(SubCommand::with_name("generate")
+                    .about("generates random game configuration")
+                    .arg(Arg::with_name("size")
+                         .short("s")
+                         .long("size")
+                         .value_name("SIZE"))
+                    .arg(Arg::with_name("solvable")
+                         .long("solvable")
+                         .value_name("SOLVABLE"))
+                    .arg(Arg::with_name("iterations")
+                         .short("i")
+                         .long("iterations")
+                         .value_name("ITERATIONS")))
+        .subcommand(SubCommand::with_name("solve")
+                    .about("solves random game configuration")
+                    .arg(Arg::with_name("file")
+                         .index(1)
+                         .value_name("FILE"))).get_matches();
 
-    // let file_name = matches.value_of("file").unwrap_or("default.map");
-    let file_name = "default.map";
-    let mut file = match File::open(file_name) {
-        Ok(f) => f,
-        Err(e) => {
-            println!("{}", e);
-            return;
+    if let Some(matches) = matches.subcommand_matches("solve") {
+
+        let file_name = matches.value_of("file").unwrap_or("default.map");
+        let mut file = match File::open(file_name) {
+            Ok(f) => f,
+            Err(e) => {
+                println!("Error: {}: {}", file_name, e);
+                return;
+            }
+        };
+
+        let mut s = String::new();
+        file.read_to_string(&mut s);
+
+        match s.parse::<node::Node>() {
+            Ok(n) => solve(n),
+            Err(e) => println!("Error: {}", e),
         }
-    };
+    } else if let Some(matches) = matches.subcommand_matches("generate") {
+        let size: usize = matches.value_of("size").unwrap_or("3").parse().unwrap_or_else(|v| {
+            println!("Warning: Invalid value provided for size: {}, usign default value (3)", v);
+            3
+        });
+        let solvable: bool = matches.value_of("solvable").unwrap_or("true").parse().unwrap_or_else(|v| {
+            println!("Warning: Invalid value provided for solvable: {}, usign default value (true)", v);
+            true
+        });
+        let iterations: usize = matches.value_of("iterations").unwrap_or("10").parse().unwrap_or_else(|v| {
+            println!("Warning: Invalid value provided for iterations: {}, usign default value (10)", v);
+            10
+        });
 
-    let mut s = String::new();
-    file.read_to_string(&mut s);
+        let n = node::Node::random(size, iterations, solvable);
+        println!("{}", n.len);
+        let mut it = n.board.iter().peekable();
 
-    match s.parse::<node::Node>() {
-        Ok(n) => solve(n),
-        Err(e) => println!("Error: {}", e),
+        while it.peek().is_some() {
+            let line: String = it.by_ref()
+                                 .take(n.len)
+                                 .map(|v| format!("{0:-2}", v))
+                                 .collect::<Vec<_>>()
+                                 .join(" ");
+            println!("{}", line);
+        }
     }
 }
 
