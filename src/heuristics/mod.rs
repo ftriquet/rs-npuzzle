69872182d387e691db35d32950e40bea66e4265a
François Triquet <francois.triquet@recast.ai>
2017-03-06 18:03:27 +0100

    add misplaced tiles heuristic

--- src/heuristics/mod.rs
+++ src/heuristics/mod.rs
@@ -3,6 +3,7 @@ use node;
 pub struct Manhattan;
 pub struct Euclide;
 pub struct LinearConflict;
+pub struct MisplacedTiles;
 
 pub trait Heuristic<T> {
     fn eval(&self, _: T) -> usize {
@@ -20,6 +21,21 @@ impl Heuristic<node::Node> for Manhattan {
             let (pos_x, pos_y) = n.get_pos(*val).unwrap();
 
             sum += (pos_x as i32 - x as i32).abs() as usize + (pos_y as i32 - y as i32).abs() as usize;
+        }
+
+        sum
+    }
+}
+
+impl Heuristic<node::Node> for MisplacedTiles {
+    fn eval(&self, n: node::Node) -> usize {
+        let mut sum = 0_usize;
+        let goal = node::Node::goal(n.len);
+
+        for (v, w) in goal.board.iter().zip(n.board.iter()) {
+            if v != w {
+                sum += 1;
+            }
         }
 
         sum
